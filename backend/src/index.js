3b96f97cb8535d7683f1ece48dfc070079da8f88
Anvith C L <anvithchakote33@gmail.com>
2026-09-03 23:41:50 +0530

    added middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
+
 const app = express();
+
+//express.json
+app.use(express.json({limit:"100mb" }));
+
+//urlencoded
+app.use(express.urlencoded({limit:"100mb",extended:true}));
+
+//cookie parser
+app.use(cookieParser());
 
 const PORT = process.env.PORT;
 
