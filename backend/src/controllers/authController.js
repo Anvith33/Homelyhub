1ab09b3fe69dd3f91b1a1a8367ef02c125607600
Anvith C L <anvithchakote33@gmail.com>
2026-09-15 00:39:37 +0530

    added signup

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -6,3 +6,19 @@ import {sendMail, forgotPassworfMailGenContent} from "../utils/mail.js";
 import {signinToken, createSendToken, defaultAvatarUrl, filterObj} from "../utils/token.js";
 
 //signup
+const signup = async (req, res) =>{
+try{
+
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    avatar: {url:req.body.avatar || defaultAvatarUrl(req.body.name)}
+  })
+
+}
+catch(error){
+
+}
+}
