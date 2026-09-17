9552a12762d72c51eb0ccaa52b8d90441a7ba1d5
Anvith C L <anvithchakote33@gmail.com>
2026-09-17 23:26:10 +0530

    completed verified payment function

--- backend/src/controllers/bookingController.js
+++ backend/src/controllers/bookingController.js
@@ -6,15 +6,69 @@ const createOrder = async(req,res)=>{
   const{amount,propertyId, fromDate,toDate,guests} = req.body;
 
   //orderID
-  const orderID="order_"+Date.now()
+  const orderId="order_"+Date.now()
   res.json({
     success:true,
     message:"Order created successfully",
-    orderID,
+    orderId,
     amount,
     propertyId,
     fromDate,
     toDate,
     guests
   })
+}
+
+//verify payment function
+const verifyPayment = async(req,res)=>{
+  const{ orderId, bookingDetails, forceStatus} = req.body;
+
+  if(forceStatus == "success")
+  {
+    const paymentId ="pay_"+Date.now();
+
+    //save booking
+    const newBooking = await Booking.create({
+      user: req.user._id,
+      property: bookingDetails.propertyId,
+      price:bookingDetails.price,
+      fromDate: bookingDetails.fromDate,
+      toDate: bookingDetails.toDate,
+      guests: bookingDetails.guests,
+      numberOfnights:bookingDetails.numberOfnights,
+      paid:true
+    });
+
+    //tell property dates are taken
+
+    const updatedProperty = await Property.findByIdAndUpdate(
+      bookingDetails.propertyId,{
+        $push:{
+          currentBookings:{
+            bookingId:newBooking._id,
+            fromDate:bookingDetails.fromDate,
+            toDate:bookingDetails.toDate,
+            userId:req.user._id
+          }
+        }
+      },
+      {new:true}
+    );
+
+    res.json({
+      success:true,
+      message:"Payment successfull booking confirmed!",
+      paymentId,
+      orderId,
+      booking:newBooking
+    })
+  }
+
+  else{
+    res.status(400).json({
+      success:false,
+      message:"payment failed",
+      orderId
+    })
+  }
 }
