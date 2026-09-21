490a9bc2202acfc33b5297a89fccb479ad2ff41b
Anvith C L <anvithchakote33@gmail.com>
2026-09-21 23:37:45 +0530

    started with frontend

---
+++ Frontend/src/store/Property/propert-slice.js
@@ -0,0 +1,12 @@
+//state manager
+// all list properties
+//count
+//search filters
+//loading flag
+//error
+
+import {createSlice} from "@reduxjs/toolkit";
+
+const propertySlice =createSlice({
+  name : "property",
+})
