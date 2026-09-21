a6e0e96d2625298926eb85ddfb80f3802b89edea
Anvith C L <anvithchakote33@gmail.com>
2026-09-21 23:45:22 +0530

    started with frontend

--- Frontend/src/store/Property/propert-slice.js
+++ Frontend/src/store/Property/propert-slice.js
@@ -9,4 +9,19 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const propertySlice =createSlice({
   name : "property",
+  initialState:{
+    properties:[],
+    totalProperties:0,
+    searchParams:{},
+    error:null,
+    loading:false
+  },
+  reducers:{
+    getRequest(state){
+      state.loading=true;
+    },
+    getProperties(state,action){
+      state.properties= action.payload.data
+    }
+  }
 })
