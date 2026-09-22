17f7aa9688479c5735fb78039db125665f83c962
Anvith C L <anvithchakote33@gmail.com>
2026-09-22 22:59:02 +0530

    added Property action to homelyhub

--- Frontend/src/store/Property/property-action.js
+++ Frontend/src/store/Property/property-action.js
@@ -10,3 +10,34 @@ import {axions} from "../../utils/axios"
 //6. get the property data
 //7.send data to redux store
 //8.if error occurs send error to redux
+
+//dispatch means send to redux
+//getstate means get from redux
+
+export const getAllProperties =()=> async(dispatchEvent, getState)=>{
+  try{
+    console.log("API call started");
+
+    dispatchEvent(propertyAction.getRequest())
+
+    const {SearchParams} = getState().properties
+
+    console.log(SearchParams)
+
+    const response = await axiosInstance.get('/v1/rent/listing',{
+      params:{...SearchParams}
+    })
+
+    if(!response){
+      throw new Error("could not fetch any properties")
+    }
+    const {data}=response;
+    console.log(data);
+
+    dispatch(propertyAction.getProperties(data))
+
+  }
+  catch(error){
+    dispatch(propertyAction.getErrors(error.message))
+  }
+}
