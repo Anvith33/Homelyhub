bdc1e152c0312116d4eec744bce327a580916ebc
Anvith C L <anvithchakote33@gmail.com>
2026-09-22 23:16:37 +0530

    modifies propertlist.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import store from "./store/store.js";
+import{Providerrovider} from"react-redux";
+import "./css/responsive.css"
 
 import "./css/responsive.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </StrictMode>
 );
