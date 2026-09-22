1b551dc65e6dc55a7d49097f04c684d1b0913c3c
Anvith C L <anvithchakote33@gmail.com>
2026-09-22 23:47:22 +0530

    debugged the error

--- Frontend/src/store/Property/property-action.js
+++ Frontend/src/store/Property/property-action.js
@@ -1,6 +1,6 @@
 import propertySlice from "./propert-slice";
 import { propertyAction } from "./propert-slice";
-import {axios} from "../../utils/axios"
+import {axiosInstance} from "../../utils/axios"
 
 //get all properties
 //1. start api req
